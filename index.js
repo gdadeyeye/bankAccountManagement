492d6c54c173c2fa78a95dbd835ecdfcc4b2296d
Gbenro Daniel Adeyeye <youthtech035@gmail.com>
2023-10-03 21:22:15 -0700

    changes made to javascript file

--- index.js
+++ index.js
@@ -18,33 +18,7 @@
         document.getElementById("account").style.display ="none";
     }
 
-    var balance = 0;
-    function  deposit() {
-    var amount = document.getElementById("SAmount").value;
-    balance =+ amount;
-    document.getElementById("accountAmtS").innerHTML = balance;
-}
-
-function  depositC() {
-    var amount = document.getElementById("CAmount").value;
-    balance =+amount;
-    document.getElementById("accountAmtC").innerHTML = balance;
-}  
-
-function  WithdrawS(){
-    if(amount <= balance){
-        balance =-amount;
-    }
-    var amount = document.getElementById("WSamount").value;
     
-    document.getElementById("accountAmtS").innerHTML = balance;
-}
-
-function  WithdrawC() {
-    var amount = document.getElementById("WCamount").value;
-    balance =-amount;
-    document.getElementById("accountAmtC").innerHTML = balance;
-}
 
         
          
