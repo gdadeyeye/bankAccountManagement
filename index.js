a1e89661e23ae71a4c43920a7d04714d6d9d151f
Gbenro Daniel Adeyeye <youthtech035@gmail.com>
2023-10-03 21:23:16 -0700

    changes made

--- index.js
+++ index.js
@@ -19,6 +19,54 @@
     }
 
     
+// this section and fuction is meant for Savings Account
+var balance = 0;
+balance = Number(balance);
+var deposit = document.getElementById('deposit');
+deposit.addEventListener('click', depositCash);
+var deposit = document.getElementById('withdrawS');
+deposit.addEventListener('click', withdrawCash);
+
+function depositCash() {
+    var depositAmt = document.getElementById('bankBalance').value ;
+    depositAmt = Number(depositAmt);
+    balance = depositAmt + balance;
+    document.getElementById("accountAmtS").innerHTML = balance;
+    document.getElementById('bankBalance').value = "";
+}
+
+function withdrawCash() {
+    var depositAmt = document.getElementById('WSamount').value ;
+    depositAmt = Number(depositAmt);
+    balance = balance - depositAmt ;
+    document.getElementById("accountAmtS").innerHTML = balance;
+    document.getElementById('WSamount').value = "";
+}
+
+
+// this section and function is meant for Checking Account
+var balance_c = 0;
+balance_c = Number(balance_c);
+var depositchecking = document.getElementById('depositc');
+depositchecking.addEventListener('click', depositCash_c);
+var depositchecking = document.getElementById('WithdrawC');
+depositchecking.addEventListener('click', withdrawCash_c);
+
+function depositCash_c() {
+    var depositAmtc = document.getElementById('bankBalc').value ;
+    depositAmtc = Number(depositAmtc);
+    balance_c = depositAmtc + balance_c;
+    document.getElementById("accountAmtC").innerHTML = balance_c;
+    document.getElementById('bankBalc').value = "";
+    }
+
+function withdrawCash_c() {
+    var depositAmtc = document.getElementById('WCamount').value ;
+    depositAmtc = Number(depositAmtc);
+    balance_c = balance_c - depositAmtc ;
+    document.getElementById("accountAmtC").innerHTML = balance_c;
+    document.getElementById('WCamount').value = "";
+}
 
         
          
